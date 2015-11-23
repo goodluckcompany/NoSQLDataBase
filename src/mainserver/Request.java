6d60057611abd62d437ffd5401ee0096a6972289
sl0nWorker <battousai45736@gmail.com>
2015-11-23 20:03:05 +0400

    simple request

--- src/mainserver/Request.java
+++ src/mainserver/Request.java
@@ -1,7 +1,21 @@
 package mainserver;
 
+import java.io.Serializable;
+
 /**
  * Created by homie on 22.11.2015.
  */
-public class Request {
+public class  Request  implements Serializable {
+    static int count = 0;
+    String name = "req";
+
+    public Request(){
+        name = name + count;
+        count++;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
