d04d346e9825cf4e06895fbb22c3080989b80fc7
sl0nWorker <battousai45736@gmail.com>
2015-11-23 20:01:39 +0400

    waiting request from main

--- src/workserver/Workserver.java
+++ src/workserver/Workserver.java
@@ -1,5 +1,6 @@
 package workserver;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.OutputStream;
@@ -7,7 +8,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.LinkedList;
 import java.util.List;
-import mainserver.Request;
+import mainserver.*;
 /**
  * Created by homie on 22.11.2015.
  */
@@ -20,36 +21,63 @@ public class Workserver {
     int PORT = 6667;
     int MAX_QUEUE = 100;
     List <NoSqlDB> listDb = new LinkedList<>();
-
+    Request r;
     public static void main(String[] args){
         new Workserver();
     }
 
     Workserver(){
+
         try {
             socketToMain = new ServerSocket(PORT,MAX_QUEUE);
-            System.out.println("Wait connect...");
+        } catch (IOException e) {
+            System.out.println("SockeToMain");
+        }
+        System.out.println("Wait connect...");
+        try {
             socket = socketToMain.accept();
+        } catch (IOException e) {
+            System.out.println("SockeToMain.accept");
+        }
 
-            InputStream sin = socket.getInputStream();
+        InputStream sin = null;
+        try {
+            sin = socket.getInputStream();
+        } catch (IOException e) {
+            System.out.println("sin");
+        }
+        try {
             OutputStream sout = socket.getOutputStream();
-           //   in = socket.getInputStream();
+        } catch (IOException e) {
+            System.out.println("sout");
+        }
+        // in = socket.getInputStream();
            // out = socket.getOutputStream();
 
-            ObjectInputStream ois = new ObjectInputStream(sin);
-            Request r = new Request();
+        ObjectInputStream ois = null;
+        try {
+            ois = new ObjectInputStream(sin);
+        } catch (IOException e) {
+            System.out.println("ois");
+        }
+
+
 
             System.out.println("Connect to be!");
-            out.write("Connect successfully!".getBytes());
+            int i = 0;
+            while ( i < 10){
 
-            while (true){
-                r = (Request)ois.readObject();
-                System.out.print(r.toString());
+                r = new Request();
+                try {
+                    r = (Request)ois.readObject();
+                    System.out.println(r.toString());
+                } catch (IOException e) {
+                    System.out.printf("r=ois");
+                } catch (ClassNotFoundException e) {
+                    System.out.println("Request not found");
+                }
+                i++;
             }
         }
-        catch (Exception e)
-        {
-            System.out.println(e);
-        }
+
     }
-}
