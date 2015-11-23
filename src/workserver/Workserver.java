5f74b6e13e14bea290c372b2c8f9f38a0ec8139a
sl0nWorker <battousai45736@gmail.com>
2015-11-23 15:29:56 +0400

    add serialization request

--- src/workserver/Workserver.java
+++ src/workserver/Workserver.java
@@ -1,10 +1,13 @@
 package workserver;
 
 import java.io.InputStream;
+import java.io.ObjectInputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-
+import java.util.LinkedList;
+import java.util.List;
+import mainserver.Request;
 /**
  * Created by homie on 22.11.2015.
  */
@@ -16,6 +19,7 @@ public class Workserver {
     int symbol;
     int PORT = 6667;
     int MAX_QUEUE = 100;
+    List <NoSqlDB> listDb = new LinkedList<>();
 
     public static void main(String[] args){
         new Workserver();
@@ -26,14 +30,21 @@ public class Workserver {
             socketToMain = new ServerSocket(PORT,MAX_QUEUE);
             System.out.println("Wait connect...");
             socket = socketToMain.accept();
-            in = socket.getInputStream();
-            out = socket.getOutputStream();
+
+            InputStream sin = socket.getInputStream();
+            OutputStream sout = socket.getOutputStream();
+           //   in = socket.getInputStream();
+           // out = socket.getOutputStream();
+
+            ObjectInputStream ois = new ObjectInputStream(sin);
+            Request r = new Request();
+
             System.out.println("Connect to be!");
             out.write("Connect successfully!".getBytes());
 
             while (true){
-                symbol = in.read();
-                System.out.print((char) symbol);
+                r = (Request)ois.readObject();
+                System.out.print(r.toString());
             }
         }
         catch (Exception e)
