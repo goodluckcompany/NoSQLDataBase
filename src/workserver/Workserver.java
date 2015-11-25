ee694d987e47a2fdd4256991dbf10ebc31305c22
sl0nWorker <battousai45736@gmail.com>
2015-11-26 00:35:58 +0400

    add ObjectOutputStream

--- src/workserver/Workserver.java
+++ src/workserver/Workserver.java
@@ -1,9 +1,6 @@
 package workserver;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.ObjectInputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.LinkedList;
@@ -12,11 +9,17 @@ import mainserver.*;
 /**
  * Created by homie on 22.11.2015.
  */
+
+
+
+
 public class Workserver {
+
+
     ServerSocket socketToMain;
     Socket socket;
     InputStream in;
-    OutputStream out;
+    OutputStream sout;
     int symbol;
     int PORT = 6667;
     int MAX_QUEUE = 100;
@@ -27,6 +30,10 @@ public class Workserver {
     }
 
     Workserver(){
+
+
+
+
 
         try {
             socketToMain = new ServerSocket(PORT,MAX_QUEUE);
@@ -47,10 +54,19 @@ public class Workserver {
             System.out.println("sin");
         }
         try {
-            OutputStream sout = socket.getOutputStream();
+            sout = socket.getOutputStream();
+
         } catch (IOException e) {
             System.out.println("sout");
         }
+
+        try {
+            ObjectOutputStream oos = new ObjectOutputStream(sout);
+            oos.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         // in = socket.getInputStream();
            // out = socket.getOutputStream();
 
@@ -65,12 +81,13 @@ public class Workserver {
 
             System.out.println("Connect to be!");
             int i = 0;
-            while ( i < 10){
+            while ( true){
 
                 r = new Request();
                 try {
                     r = (Request)ois.readObject();
                     System.out.println(r.toString());
+
                 } catch (IOException e) {
                     System.out.printf("r=ois");
                 } catch (ClassNotFoundException e) {
