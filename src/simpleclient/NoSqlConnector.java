8dd62ef6aba49019ff93e15ac9ec357a7b38c9dc
Shirru <annafreia@mail.ru>
2015-12-13 23:21:50 +0300

    added NoSqlConnector class

---
+++ src/simpleclient/NoSqlConnector.java
@@ -0,0 +1,86 @@
+package simpleclient;
+
+import mainserver.Request;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+/**
+ * Created by Anna on 13.12.2015.
+ */
+public class NoSqlConnector {
+    Socket socket;
+    Request response;
+    ObjectOutputStream oos;
+    ObjectInputStream ois;
+    int MAIN_PORT = 6661;
+    String mainServerIP;
+
+    NoSqlConnector(String ip){
+        this.mainServerIP = ip;
+    }
+
+    int establishConnection(){
+        try {
+            this.socket = new Socket(mainServerIP, MAIN_PORT);
+        } catch (IOException e) {
+            System.err.println(e);
+            return 1;
+        }
+        try {
+            oos = new ObjectOutputStream(socket.getOutputStream());
+            oos.flush();
+        } catch (IOException e) {
+            System.err.println(e);
+            return 2;
+        }
+        return 0;
+    }
+    
+    Request sendCommand(String command){
+        if(socket.isConnected()){
+            try{
+                oos.writeObject(command);
+            } catch (IOException e) {
+                System.err.println(e);
+            }
+            try {
+                ois = new ObjectInputStream(socket.getInputStream());
+            } catch (IOException e) {
+                System.err.println(e);
+            }
+            try{
+                response = (Request) ois.readObject();
+            } catch (ClassNotFoundException e) {
+                System.err.println(e);
+            } catch (IOException e) {
+                System.err.println(e);
+            }
+        }
+        return response;
+    }
+
+    int breakConnection(){
+        try {
+            oos.close();
+        } catch (IOException e) {
+            System.err.println(e);
+            return 1;
+        }
+        try {
+            ois.close();
+        } catch (IOException e) {
+            System.err.println(e);
+            return 2;
+        }
+        try {
+            socket.close();
+        } catch (IOException e) {
+            System.err.println(e);
+            return 3;
+        }
+        return 0;
+    }
+}
