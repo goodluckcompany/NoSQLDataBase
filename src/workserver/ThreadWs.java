a3281caa981c5ac047de694e18d9a53f8e0eb2b0
sl0nWorker <battousai45736@gmail.com>
2015-12-19 19:56:00 +0400

    added javadoc for ThreadWs

--- src/workserver/ThreadWs.java
+++ src/workserver/ThreadWs.java
@@ -12,11 +12,29 @@ import java.util.ListIterator;
  * Created by Anderson on 26.11.2015.
  */
 public class ThreadWs implements Runnable {
+
+    /**  ласс служит дл€ создани€ патока рабочего сервера, который обрабатывает запрос с мейна, возвраща€ ответ в виде Request:
+     * <br>
+     * ¬ списке таблиц происходит поиск таблицы указанной в поле nameTable, класса Request
+     * <br>
+     * «атем вызвываетс€ парсер которому передаетс€ nosql запрос, который хранитс€ в Request
+     * <br>
+     * ѕарсер вызывает соответствующую функцию измени€,создани€ таблицы и возвращает ответ
+     * <br>
+     * «атем этот ответ(Request) передаетс€ в выходной поток, который считывает главный сервер
+     * <br>
+     * {@link ThreadWs#ipAdress}, {@link ThreadWs#req}, {@link ThreadWs#t},
+     * {@link ThreadWs#oos}.
+     * @author Kurishev Oleg
+     */
+
+    /** »спользуетс€ дл€ указани€ адресса клиента от которого пришел запрос*/
     String ipAdress;
+    /** »спользуетс€ дл€ заполнени€ ответного Request*/
     Request req;
+    /** »спользуетс€ дл€ создани€ потока*/
     Thread t;
     public ObjectOutputStream oos;
-
     ThreadWs(Request r, ObjectOutputStream _oos,List<NoSqlDB> listDb, ResponseItem items){
         ArrayList <String> list = new ArrayList<>();
         req = r;
@@ -25,7 +43,6 @@ public class ThreadWs implements Runnable {
         String nosqlR = req.getNosqlR();
         NoSqlParser nsp = new NoSqlParser();
         ResponseItem is = null;
-
         ListOfTables lt = new ListOfTables();
         list = lt.getTables();
         if (!list.isEmpty()) {
@@ -71,7 +88,6 @@ public class ThreadWs implements Runnable {
         }
 
     }
-
     static public int tableNum(String nametable,List<NoSqlDB> listDb){
         int i = 0;
         NoSqlDB ndb = null;
@@ -85,6 +101,4 @@ public class ThreadWs implements Runnable {
         }
         return -1;
     }
-
-
 }
